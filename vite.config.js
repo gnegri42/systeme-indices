15a1c62a0dde93ce2020cdab21aeea27952c716e
Geoffrey Negri <contact@geoffreynegri.fr>
2024-10-07 22:31:22 +0200

    Changement config vite pour accepter connexions externe

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0", // Permet l'accès depuis n'importe quel appareil du réseau
+    port: 5173, // Assurez-vous que le port est bien 5173
+  },
+});
