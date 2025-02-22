6a5b5451ab7478bb1383630ed5ddbf23e96cee33
Universcape <109909233+Universcape@users.noreply.github.com>
2025-02-22 16:21:00 +0100

    Update PlayerPage.jsx

--- src/pages/PlayerPage/PlayerPage.jsx
+++ src/pages/PlayerPage/PlayerPage.jsx
@@ -6,6 +6,7 @@ import useFullScreen from "../../hooks/FullScreen";
 import useDisableShortcuts from "../../hooks/DisableShortcuts";
 import "./PlayerPage.css";
 import video from "../../assets/background.mp4";
+import avatar from "../../assets/sivrage_avatar.png";
 import { mainUrl } from "../../config/config";
 
 const PlayerPage = () => {
